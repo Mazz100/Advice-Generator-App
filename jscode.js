05dda94c547caa93d8f6b11d7c5aff2fae97d6de
Mazen Hassan <95134348+Mazz100@users.noreply.github.com>
2024-02-02 19:12:27 +0200

    Animation and README update

--- jscode.js
+++ jscode.js
@@ -1,9 +1,22 @@
 const adviceId = document.querySelector('#number-id');
 const adviceText = document.querySelector('#advice');
 const adviceBTN = document.querySelector('button');
+const diceImage = document.querySelector('.dice-image');
+//Create animation for dice image
+const diceRotateAnimation = [
+    { transform: "rotate(0)" },
+    { transform: "rotate(0.5turn)" },
+];
+const diceRotateTiming = {
+    duration: 300,
+    iterations: 1,
+}
 
 adviceBTN.addEventListener("click", () => {
     FetchAdvice();
+ 
+    //Animate dice when button clicked
+    diceImage.animate(diceRotateAnimation, diceRotateTiming);
 });
 
 FetchAdvice();
@@ -17,13 +30,13 @@ async function FetchAdvice() {
         if (!response.ok) {
             throw new Error("Could not fetch resources");
         }
-        
+
         const data = await response.json();
 
         // Accessing the parent object before targeting a specific object array
         const advice = data.slip.advice;
         const id = data.slip.id;
-       
+
         adviceText.textContent = `"${advice}"`;
         adviceId.textContent = id;
     }
