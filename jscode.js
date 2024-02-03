2d72fa59839a4b187b4960636d23c2ca7cc5f8ba
Mazen Hassan <95134348+Mazz100@users.noreply.github.com>
2024-02-03 20:05:36 +0200

    Accessibility update

--- jscode.js
+++ jscode.js
@@ -12,38 +12,45 @@ const diceRotateTiming = {
     iterations: 1,
 }
 
-adviceBTN.addEventListener("click", () => {
-    FetchAdvice();
- 
+function animateDice() {
     //Animate dice when button clicked
     diceImage.animate(diceRotateAnimation, diceRotateTiming);
-});
-
-FetchAdvice();
-
-// Fetching API using await method
-async function FetchAdvice() {
-    try {
-        const response = await fetch("https://api.adviceslip.com/advice");
-
-        //Check if response is failed
-        if (!response.ok) {
-            throw new Error("Could not fetch resources");
-        }
-
-        const data = await response.json();
-
-        // Accessing the parent object before targeting a specific object array
-        const advice = data.slip.advice;
-        const id = data.slip.id;
-
-        adviceText.textContent = `"${advice}"`;
-        adviceId.textContent = id;
-    }
-
-    catch (error) {
-        console.error(error)
-    }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchAdvice();
+
+    // Fetching API using await method
+    async function fetchAdvice() {
+        try {
+            const response = await fetch("https://api.adviceslip.com/advice");
+    
+            //Check if response is failed
+            if (!response.ok) {
+                throw new Error("Could not fetch resources");
+            }
+    
+            const data = await response.json();
+    
+            // Accessing the parent object before targeting a specific object array
+            const advice = data.slip.advice;
+            const id = data.slip.id;
+    
+            adviceText.textContent = `"${advice}"`;
+            adviceId.textContent = id;
+        }
+    
+        catch (error) {
+            console.error(error)
+        }
+    }
+
+    adviceBTN.addEventListener("click", () => {
+        fetchAdvice();
+        animateDice();
+    });
+    
+})
+
 
 
